2fbfb6920d47a281de6ba0086104c95a91586265
Edisson <edisson072011@gmail.com>
2022-11-30 21:56:04 -0600

    Division de Trabajo

---
+++ racks.php
@@ -0,0 +1,45 @@
+<?php 
+require_once 'config/db.php';
+require_once 'config/conexion.php';
+require_once 'php/accesos.php';
+require_once 'php/funciones.php';
+
+$titulo = "RACKS";
+?>
+<!DOCTYPE html>
+<html lang="es">
+<?php include_once 'head.php';?>
+<body class="skin-default fixed-layout">   
+    <?php include_once 'loader.php';?>
+    <div id="main-wrapper">
+        <?php 
+        include_once 'top_bar.php';
+        include_once 'navbar.php';
+        ?>
+        <div class="page-wrapper">
+            <div class="container-fluid">
+                <div class="row page-titles">
+                    <div class="col-md-12">
+                        <h4 class="text-white"><?php echo $titulo;?></h4>
+                    </div>
+                    <div class="col-md-6 text-right">
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                This is some text within a card block.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <?php include_once 'rightbar.php';?>
+            </div>
+        </div>
+        <?php include_once 'footer.php';?>
+    </div>
+    <?php include_once 'scripts.php'; ?>
+</body>
+</html>
