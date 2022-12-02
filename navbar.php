ef1a6d0b9a1ebff5da26fdbe061d82ec6feff64f
Edisson <edisson072011@gmail.com>
2022-12-01 21:13:16 -0600

    NavBar

--- navbar.php
+++ navbar.php
@@ -4,11 +4,37 @@
     <li>
         <a href="javascript:void(0)"><i class="icon-speedometer"></i></a>
         <div class="sidebarmenu">
-            <h3 class="menu-title">Panel</h3>
+            <h3 class="menu-title">PANEL</h3>
             <ul class="sidebar-menu">
+<<<<<<< Updated upstream
                 <li><a href="equipos.php">EQUIPOS </a></li>
                 <li><a href="rol.php">ROL </a></li>
                 <li><a href="sucursal.php">sucursal </a></li>
+=======
+                <li><a href="index.php">Pamel</a></li>
+                <li><a href="base.php">Base</a></li>
+                <li><a href="rol.php">Rol</a></li>
+            </ul>
+        </div>
+    </li>
+    <li>
+        <a href="javascript:void(0)"><i class="icon-briefcase"></i></a>
+        <div class="sidebarmenu">
+            <h3 class="menu-title">Sucursales</h3>
+            <ul class="sidebar-menu">
+                <li><a href="mapa.php">Mapa</a></li>
+            </ul>
+        </div>
+    </li>
+    <li>
+        <a href="javascript:void(0)"><i class="icon-grid"></i></a>
+        <div class="sidebarmenu">
+            <h3 class="menu-title">Racks</h3>
+            <ul class="sidebar-menu">
+                <li><a href="rack_nuevo.php">Crear</a></li>
+                <li><a href="rack_editar.php">Editar</a></li>
+                <li><a href="racks.php">Rack</a></li>
+>>>>>>> Stashed changes
             </ul>
         </div>
     </li>
