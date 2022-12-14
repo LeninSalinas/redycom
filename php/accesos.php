8edff4f62360c9fad775b29b7ffc9a7612ffe6a5
Edisson <edisson072011@gmail.com>
2022-12-14 17:53:20 -0600

    Bloqueo de Paginas

--- php/accesos.php
+++ php/accesos.php
@@ -1,6 +1,15 @@
 <?php
 date_default_timezone_set('America/Guatemala');
 
+session_start();
+if (!isset($_SESSION['user_login_status'])) {
+    header('location: login.php');
+}else{
+    $id_usr = $_SESSION['id_usr'];
+    $nivel_usr = $_SESSION['nivel_usr'];
+    $fullnameUser = $_SESSION['f_name'];
+}
+
 
 
 ?>
